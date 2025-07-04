df9dbf01247c44f9b114f4f81e38df7289833028
Andy Tran <hello@andytran.me>
2025-07-04 00:45:38 -0700

    Remove boilerplate code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import { Button } from '~/components/ui/button';
-export type test = {
-  name: string;
-  age: number;
-};
-
 export default function Home() {
-  return <Button>123</Button>;
+  return (
+    <div>
+      <h1>Pokedex Tracker</h1>
+    </div>
+  );
 }
